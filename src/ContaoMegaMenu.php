fb9e9f63873712f771799b169fad99f8409d9849
stefansl <ssl@clickpress.de>
2024-09-24 15:37:56 +0200

    Remove obsolete configuration files from mega menu module
    Deleted .htaccess, autoload.ini, and autoload.php files from the contao-mega-menu module as they are no longer needed. This cleanup helps reduce clutter and potential confusion in the configuration files.

---
+++ src/ContaoMegaMenu.php
@@ -0,0 +1,18 @@
+<?php
+ namespace Derhaeuptling\ContaoMegaMenu;
+
+ use Symfony\Component\DependencyInjection\ContainerBuilder;
+ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+ class ContaoMegaMenu extends AbstractBundle
+ {
+     public function loadExtension(
+         array $config,
+         ContainerConfigurator $container,
+         ContainerBuilder $builder,
+     ): void
+     {
+         $container->import('../config/services.yaml');
+     }
+ }
