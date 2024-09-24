d4af11621992e913e9f6fbf58fdd8c0b880b1f81
stefansl <ssl@clickpress.de>
2024-09-24 16:12:26 +0200

    Update bundle class reference in Plugin.php
    Replaced `ContaoMegaMenu` with `ContaoMegaMenuBundle` in `Plugin.php`. This change ensures that the correct bundle class is being used, improving compatibility and functionality.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -5,14 +5,14 @@ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\CoreBundle\ContaoCoreBundle;
-use Derhaeuptling\ContaoMegaMenu\ContaoMegaMenu;
+use Derhaeuptling\ContaoMegaMenu\ContaoMegaMenuBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(ContaoMegaMenu::class)
+            BundleConfig::create(ContaoMegaMenuBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
