db0b8ed49a6b57086cb893086b59c9e6a60e9808
stefansl <ssl@clickpress.de>
2024-09-26 12:40:06 +0200

    Refactor: Use static closure for input field callback
    Updated the 'input_field_callback' to a static closure in 'tl_page.php' to improve performance and clarity. This change ensures that the callback does not rely on any instance properties or methods.

--- src/EventListener/DataContainer/OptionsCallback.php
+++ src/EventListener/DataContainer/OptionsCallback.php
@@ -5,7 +5,7 @@ use Contao\Controller;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\DataContainer;
 
-#[AsCallback('tl_content', 'fields.template.options')]
+#[AsCallback('tl_mega_menu', 'fields.template.options')]
 class OptionsCallback
 {
     public function __invoke(DataContainer|null $dc = null): array
